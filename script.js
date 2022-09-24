9286f3b9320fe1d6ef087374599ca51388272397
Tahlia <tbolden13@gmail.com>
2022-09-23 19:12:29 -0700

    trying to fix layout and code

---
+++ script.js
@@ -0,0 +1,27 @@
+let apiKey="c3896b557cd23f558ec16fa374b09d61"
+let todaysDate = moment(). format('1');
+let cityInputEl = document.querySelector('#city');
+let citySearch = [];
+let dataFormEl = document.querySelector('#data-form');
+let listEl - document.getElementById('myData');
+/*
+function citySearch () {
+let city = $('#city').val();
+fetch var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+}
+*/
+
+fetch (apiKey)
+.then(function(response) {
+    //convert to JSON object
+    return response.json();
+})
+.then(funtion(data) {
+    //display html here
+    let  = data.response.docs;
+    for (let i = 0; i< docArray.length; i ++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = docArray[i].city;
+        listEl.appendChild(listItem);
+    })
+
