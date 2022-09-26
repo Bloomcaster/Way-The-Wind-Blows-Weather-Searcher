5284594334d00a56e7ef21c6ae46d2f621016395
Tahlia <tbolden13@gmail.com>
2022-09-26 01:09:51 -0700

    trying to get the weather icons to work and local storage

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let apiKey="c3896b557cd23f558ec16fa374b09d61"
-let todaysDate = moment(). format('1');
-let cityInputEl = document.querySelector('#city');
-let citySearch = [];
-let dataFormEl = document.querySelector('#data-form');
-let listEl - document.getElementById('myData');
-var timeDisplayEl = $('#time-display');
-
+const apiKey="c3896b557cd23f558ec16fa374b09d61"
+const dateEl = moment(). format('1');
+const cityInputEl = document.querySelector('#city');
+const city = [];
+const timeEl = getElementById('time');
+const weatherForecastDayEL = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById("current-temperature");
 
 var currentTime = moment().format("LLLL");
-$("#currentDay").text(moment().format("LLLL"))
+$("#time").text(moment().format("LLLL"))
 /*
 function citySearch () {
 let city = $('#city').val();
 fetch var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 }
 */
-
+/*
 fetch (apiKey)
 .then(function(response) {
     //convert to JSON object
@@ -28,5 +27,5 @@ fetch (apiKey)
         let listItem = document.createElement("li");
         listItem.textContent = docArray[i].city;
         listEl.appendChild(listItem);
-    })
+    }) */
 
