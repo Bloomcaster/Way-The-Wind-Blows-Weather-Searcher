805e9c7e7ab3bd3919bcac0b9c83b3773772acdd
Tahlia <tbolden13@gmail.com>
2022-09-26 14:38:32 -0700

    working of js, dev tools are not showing data

--- script.js
+++ script.js
@@ -5,10 +5,64 @@ const city = [];
 const timeEl = getElementById('time');
 const weatherForecastDayEL = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById("current-temperature");
-
-var currentTime = moment().format("LLLL");
-$("#time").text(moment().format("LLLL"))
+const currentWeatherItemsEl = getElementById('current-weather-items');
 /*
+const currentTime = moment().format("LLLL");
+$("#time").text(moment().format("LLLL"))*/
+
+const months =['January', 'February', 'March', 'April', 'May', 'June', 
+               'July', 'August', 'September', 'October', 'November', 'December'];
+ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] ;             
+
+
+
+setInterval ( ) => {
+
+    const month = time.getMonth ();
+    const day = time.getDay ();
+    const date = time.getDate ();
+    const hour = time.getHours ();
+    const minutes = time.getMinutes ();
+    timeEl.innerHTML = hourin24hourformat + ':' + minutes;
+    dateEl.innerHTML = day[day] + ',' + date + ' ' + months[months]
+
+} 1000;
+/*dev tools are not working, check on why*/
+populateWeatherData ()
+function populateWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        let {latitude, longitude } = success.coords;
+/*this has been changed to fetch longitude, lat, city and to change unites to imperial for farenheit*/
+        fetch(`https://api.openweathermap.org.data/2.5/onecall?lat=${latitude}&lon=${longitude}&city.name=${city}&exclude
+        =hourly,minutely&unites=imperial&appid=${apiKey}`).then(res => res.json()).then(data => {
+            console.log(data)
+            showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData(data) {
+    let 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* keeping these wriiten functions in case
 function citySearch () {
 let city = $('#city').val();
 fetch var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
