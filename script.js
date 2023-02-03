dc86baf595f6fe17f56b2b250cfb7e3f9abf77c1
Tahlia <tbolden13@gmail.com>
2023-02-02 17:15:35 -0800

    js for the weather app, for data pulled from api

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const apiKey="c3896b557cd23f558ec16fa374b09d61"
 
+let gotPosition = function(pos) {
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    getForecast(lat, long);
+}
 
- function GetInfo() {
-    const newName = document.getElementById("cityInput");
-    const cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--"
- } 
+let getForecast = function(lat, long) {
+    let url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+    getWeatherText(url);
+}
 
- fetch("api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid={API key}"
- .themn(response => response.json())
- .then(data => {
-    for(i=0;i<5;i++){
-        document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp) 
+async function getWeatherText(url) {
+    let weatherObject = await fetch(url);
+    let weatherText = await weatherObject.text();
+    parseWeather(weatherText);
+}
+
+let parseWeather = function(weatherText) {
+    let weatherJSON = JSON.parse(weatherText);
+    let dailyForecast = weather.JSON.daily;
+
+    for (x = 0; x< dailyForecast.legnth; x++) {
+        let day = dailyForecast[x];
+        let today = new Date().getDay() + x;
+        
     }
- })
+    let dayOfWeek = getDayOfWeek(today);
+    let description = day.weather[0].description;
+    let icon = day.weather[0].icon;
+    let sunrise = timestampToTime(day.sunrise);
+    let sunset = timestampToTime(day.sunset);
+}
+
+
+
     
  
 
