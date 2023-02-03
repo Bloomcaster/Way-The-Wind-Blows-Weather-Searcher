850dedc68c3987540ec5f944176e03f2d536aab5
Tahlia <tbolden13@gmail.com>
2023-02-02 23:23:20 -0800

    fingers crossed that the api registers the actual city

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener("load",()=>{
             }).then((data)=>{
                 console.log(data);
                 console.log(new Date().getTime())
-                var dat= new Date(data.dt)
+                var dat = new Date(data.dt)
                 console.log(dat.toLocaleString(undefined,'Asia/Kolkata'))
                 console.log(new Date().getMinutes())
                 weatherReport(data);
@@ -20,7 +20,77 @@ window.addEventListener("load",()=>{
     }
 })
 
+function searchByCity(){
+    var place= document.getElementById('input').value;
+    var urlsearch= `http://api.openweathermap.org/data/2.5/weather?q=${place}&` + `appid=${apikey}`;
 
+    fetch(urlsearch).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        console.log(data);
+        weatherReport(data);
+    })
+    document.getElementById('input').value='';
+}
+
+function weatherReport(data){
+
+    var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
+
+    fetch(urlcast).then((res)=>{
+        return res.json();
+    }).then((forecast)=>{
+        console.log(forecast.city);
+        hourForecast(forecast);
+        dayForecast(forecast)
+
+        console.log(data);
+        document.getElementById('city').innerText= data.name + ', '+data.sys.country;
+        console.log(data.name,data.sys.country);
+    
+        console.log(Math.floor(data.main.temp-273));
+        document.getElementById('temperature').innerText= Math.floor(data.main.temp-273)+ ' °C';
+    
+        document.getElementById('clouds').innerText= data.weather[0].description;
+        console.log(data.weather[0].description)
+        
+        //let icon1= data.weather[0].icon;
+        //let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+      //  document.getElementById('img').src=iconurl
+    })
+
+}
+
+function hourForecast(forecast){
+    document.querySelector('.templist').innerHTML=''
+    for (let i = 0; i < 5; i++) {
+
+        var date= new Date(forecast.list[i].dt*1000)
+        console.log((date.toLocaleTimeString(undefined,'Asia/Kolkata')).replace(':00',''))
+
+        let hourR=document.createElement('div');
+        hourR.setAttribute('class','next');
+
+        let div= document.createElement('div');
+        let time= document.createElement('p');
+        time.setAttribute('class','time')
+        time.innerText= (date.toLocaleTimeString(undefined,'North America/Seattle')).replace(':00','');
+
+        let temp= document.createElement('p');
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
+
+        div.appendChild(time)
+        div.appendChild(temp)
+
+        let desc= document.createElement('p');
+        desc.setAttribute('class','desc')
+        desc.innerText= forecast.list[i].weather[0].description;
+
+        hourR.appendChild(div);
+        hourR.appendChild(desc)
+        document.querySelector('.templist').appendChild(hourR);
+}
+}
 
     
  
