61f289241f002394512b0d8b54a78056be6777a5
Tahlia <tbolden13@gmail.com>
2023-02-02 16:56:19 -0800

    styling of containers

--- script.js
+++ script.js
@@ -1,98 +1,24 @@
 const apiKey="c3896b557cd23f558ec16fa374b09d61"
-const dateEl = moment(). format('1');
-const cityInput = document.querySelector('#city');
-const timeEl = querySelector('#time');
-const weatherForecastDayEL = document.querySelector('#weather-forecast');
-const currentTempEl = document.querySelector('#current-temperature');
-const currentWeatherItemsEl = querySelector('#weather-forecast');
-
-const currentTime = moment().format("LLLL");
-$("#time").text(moment().format("LLLL"))
-
-const months =['January', 'February', 'March', 'April', 'May', 'June', 
-               'July', 'August', 'September', 'October', 'November', 'December'];
- const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] ;             
 
 
+ function GetInfo() {
+    const newName = document.getElementById("cityInput");
+    const cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--"
+ } 
 
-setInterval (() => {
-
-    const month = time.getMonth ();
-    const day = time.getDay ();
-    const date = time.getDate ();
-    const hour = time.getHours ();
-    const minutes = time.getMinutes ();
-    timeEl.innerHTML = hourin24hourformat + ':' + minutes;
-    dateEl.innerHTML = day[day] + ',' + date + ' ' + months[months]
-
-}, 1000);
-
-const formSubmitHandler = function (event){
-    event.preventDefault();
-
-    let city = cityInput.value;
-
-    if (city) {
-        newQuery(city);
-
-        cityInput.value = '';
-    } else {
-        alert("oh nooooo");
+ fetch("api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid={API key}"
+ .themn(response => response.json())
+ .then(data => {
+    for(i=0;i<5;i++){
+        document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp) 
     }
-    };
-
-
-const newQuery = function city () {
-    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-
-   /*
-    fetch(apiKey)
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data){
-                
-                displayWeather(data, city);
-            })
-        .then(data => {
-            localStorage.setItem('city',
-           
-            json.stringify(data))
-            console.log(data);
-        })
-    }
-}*/
-
-  fetch(apiUrl)
- .then(function(response) {
-     response.json().then(function (data){
-        console.log(data);
-        displayWeather(data, city);
-       }); 
-  
-
-    /*
-       function showWeatherData (data) {
-        let {wind_speed, humidity, temperature} = data.current;
-
-
-
-        currentWeatherItemsEl.innerHTML =
-      ` <div class="stormy" id="all-weather-items">
-       <div class="weather-item">
-         <div>Wind Speed</div>
-         <div>${wind_speed}</div>
-       </div>
-
-       <div class="weather-item">
-         <div>Humidity</div>
-         <div>${humidity}%</div>
-       </div>
-
-      <div class="weather-item">
-         <div>Temperature</div>
-         <div>${temperature}</div>
-       </div>;
+ })
     
+ 
+
+
+
 
 
 
