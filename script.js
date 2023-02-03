73a4c1daa9957759d1e60cfb83f4679126715192
Tahlia <tbolden13@gmail.com>
2023-02-02 23:58:44 -0800

    trying to fix bugs, citys wont change but app asks for location

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const apiKey="c3896b557cd23f558ec16fa374b09d61"
 window.addEventListener("load",()=>{
     if(navigator.geolocation){
@@ -54,9 +55,9 @@ function weatherReport(data){
         document.getElementById('clouds').innerText= data.weather[0].description;
         console.log(data.weather[0].description)
         
-        //let icon1= data.weather[0].icon;
-        //let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
-      //  document.getElementById('img').src=iconurl
+        let icon1= data.weather[0].icon;
+        let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+        document.getElementById('img').src=iconurl
     })
 
 }
@@ -91,6 +92,31 @@ function hourForecast(forecast){
         document.querySelector('.templist').appendChild(hourR);
 }
 }
+
+function dayForecast(forecast){
+    document.querySelector('.weekF').innerHTML=''
+    for (let i = 8; i < forecast.list.length; i+=8) {
+        console.log(forecast.list[i]);
+        let div= document.createElement('div');
+        div.setAttribute('class','dayF');
+        
+        let day= document.createElement('p');
+        day.setAttribute('class','date')
+        day.innerText= new Date(forecast.list[i].dt*1000).toDateString(undefined,'Asia/Kolkata');
+        div.appendChild(day);
+
+        let temp= document.createElement('p');
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
+        div.appendChild(temp)
+
+        let description= document.createElement('p');
+        description.setAttribute('class','desc')
+        description.innerText= forecast.list[i].weather[0].description;
+        div.appendChild(description);
+
+        document.querySelector('.weekF').appendChild(div)
+    }
+} 
 
     
  
